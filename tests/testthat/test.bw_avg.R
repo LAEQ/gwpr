c36e21b636a5e8174cad82d3ebae80abb57f65ae
David Maignan <davidmaignan@gmail.com>
2020-05-06 17:16:00 -0500

    no message

--- tests/testthat/test.bw_avg.R
+++ tests/testthat/test.bw_avg.R
@@ -3,18 +3,21 @@
 library(testthat)
 
 test_that("Bandwith option: Version avec donnees moyennes / adaptive bw ", {
-  path <- file.path("data", "Data.rda")
+  path <- file.path(getwd(), "..","..", "data", "Data.rda")
   print(path)
 
-  base::load(file = path)
-  USStates@data$id <- c(1:length(unique(USStates@data[,"state"])))
-  data <- merge(USStates@data, Produc, by="state", all=T)
+  # base::load(file = path)
+  # USStates@data$id <- c(1:length(unique(USStates@data[,"state"])))
+  # data <- merge(USStates@data, Produc, by="state", all=T)
+  #
+  # dMat <- GWmodel::gw.dist(sp::coordinates(USStates),  p=2, longlat=F)
+  # Equation <- log(gsp) ~ log(pcap) + log(pc) + log(emp) + unemp
+  #
+  # bwAVG.A <- bw.avg(formula=Equation, data=data, SDF=USStates, index=c("id","year"), approach="AICc",  kernel="bisquare", adaptive=T, p=2, longlat=F, dMat=dMat)
+  #
+  # expect_equal(bwAVG.A, 37)
 
-  dMat <- GWmodel::gw.dist(sp::coordinates(USStates),  p=2, longlat=F)
-  Equation <- log(gsp) ~ log(pcap) + log(pc) + log(emp) + unemp
+  expect_equal(path, "test/path")
 
-  bwAVG.A <- bw.avg(formula=Equation, data=data, SDF=USStates, index=c("id","year"), approach="AICc",  kernel="bisquare", adaptive=T, p=2, longlat=F, dMat=dMat)
-
-  expect_equal(bwAVG.A, 37)
 })
 
