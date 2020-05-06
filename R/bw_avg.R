dcd81837595fac3d9492dc9cfcbe774fa4c778ac
David Maignan <davidmaignan@gmail.com>
2020-05-06 14:41:33 -0500

    fix appveyor warnings: bw.avg

--- R/bw_avg.R
+++ R/bw_avg.R
@@ -1,6 +1,3 @@
-library(plm)
-# library(GWmodel)
-
 #' A function for bandwith selection to calibrate a GWPR model, based on the mean over time of the data.
 #' The formula for calculating the CV value for Panel is based on the study of:
 #' YU, Danlin. 2010. Exploring spatiotemporally varying regressed relationships: the geographically weighted panel regression analysis.
@@ -22,29 +19,29 @@ library(plm)
 #' @export
 bw.avg <- function(formula, data, SDF, index, approach=c("CV","AICc"), kernel="bisquare", adaptive=FALSE, p=2, longlat=FALSE, dMat){
   #Extraction of parameters to be used
-  pdata <- pdata.frame(data, index = index, drop.index = FALSE, row.names = FALSE, stringsAsFactors = default.stringsAsFactors())
-  panelModel <- plm(formula = formula, model="pooling", data=pdata, index=index)
-  n <- length(unique(pdata[,index[1]]))
-  t <- length(unique(pdata[,index[2]]))
-  K <- length(all.vars(formula)[-1])
-  y <- pmodel.response(panelModel) #allows for keeping variable transformations (log, ^x, ...)
-  x <- model.matrix(panelModel)[,-1]
+  pdata       <- plm::pdata.frame(data, index = index, drop.index = FALSE, row.names = FALSE, stringsAsFactors = default.stringsAsFactors())
+  panelModel  <- plm(formula = formula, model="pooling", data=pdata, index=index)
+  n           <- length(unique(pdata[,index[1]]))
+  t           <- length(unique(pdata[,index[2]]))
+  K           <- length(all.vars(formula)[-1])
+  y           <- plm::pmodel.response(panelModel) #allows for keeping variable transformations (log, ^x, ...)
+  x           <- stats::model.matrix(panelModel)[,-1]
 
   #Verification that the panel dataset is balanced
   if(dim(pdata)[1]!=n*t) stop("Averaging process available only for balanced panels")
 
   #Averaging the dataset over time
-  idx <- as.numeric(rep(seq(1,n),each=t))
-  yAVG <- tapply(y,idx,mean)
-  tpms <- function(q) tapply(q,idx,mean)
-  xAVG <- apply(x,2,tpms)
-  dataAVG <- as.data.frame(cbind(yAVG,xAVG))
+  idx       <- as.numeric(rep(seq(1,n),each=t))
+  yAVG      <- tapply(y,idx,mean)
+  tpms      <- function(q) tapply(q,idx,mean)
+  xAVG      <- apply(x,2,tpms)
+  dataAVG   <- as.data.frame(cbind(yAVG,xAVG))
   colnames(dataAVG) <- c(formula[[2]], colnames(x))
-  SDF@data <- dataAVG
+  SDF@data  <- dataAVG
 
   #Optimization process with transformed data (left without time dimension).
-  formulaAVG <- formula(dataAVG) #Creation of a new formula to avoid transforming variables a second time (ex. log(log(X)), ...)
-  bw <- GWmodel::bw.gwr(formula=formulaAVG, data=SDF, approach=approach, kernel=kernel, adaptive=adaptive, p=p, longlat=longlat, dMat=dMat)
+  formulaAVG  <- formula(dataAVG) #Creation of a new formula to avoid transforming variables a second time (ex. log(log(X)), ...)
+  bw          <- GWmodel::bw.gwr(formula=formulaAVG, data=SDF, approach=approach, kernel=kernel, adaptive=adaptive, p=p, longlat=longlat, dMat=dMat)
   return(bw)
 }
 
@@ -62,63 +59,59 @@ bw.avg <- function(formula, data, SDF, index, approach=c("CV","AICc"), kernel="b
 #' @return list(bandwidth = double)
 #'
 #' @export
-bw.CV.A <- function(formula, data, index, effect=c("individual", "time", "twoways", "nested"),
-                    model=c("within", "random", "ht", "between", "pooling", "fd"),
-                    kernel="bisquare", dMat, bws){
-
-  # require(plm)
-  # require(GWmodel)
-
-  #Data preparation
-  Pdata <- pdata.frame(data, index = index, drop.index = FALSE, row.names = FALSE,
-                       stringsAsFactors = default.stringsAsFactors())
-  n <- length(unique(Pdata[,index[1]]))
-  t <- length(unique(Pdata[,index[2]]))
-
-  CVsMat <- matrix(NA, nrow = length(bws), ncol = 2)
-  colnames(CVsMat) <- c("Bandwidth", "CV-score")
-  resid <- c()
-
-  # Faire ici le critere de mesure du data pour, si n*t > C, impression d'un message tel quel
-  # "Long data, calculations may take a long time, optimisation on time-averaged data suggested
-  # by choosing 'short = TRUE'." et auquel cas moyenner les donnees et les passer dans bw.avg
-
-  #Optimization Process
-  cat("\nOptimization Process:", length(bws), "iterations \n")
-  pb <- txtProgressBar(min = 0, max = length(bws), style = 3)
-
-  for(i in 1:length(bws)){
-    bw <- bws[i]
-
-    for(j in 1:n){
-      W.j <- GWmodel::gw.weight(as.numeric(dMat[j,]), bw=bw, kernel=kernel, adaptive=T)
-      W.j[j] <- 0
-      W.j <- diag(W.j)
-      W.j <- kronecker(W.j,diag(t))
-      W.j <- diag(W.j)
-      Pdata$wgt <- W.j
-      plm.j <- plm::plm(formula=formula, model=model, data=Pdata, index=index, weights=wgt)
-
-      for(l in 1:t){
-        resid[(j-1)*t+l] <- pmodel.response(plm.j)[(j-1)*t+l] - predict(plm.j)[(j-1)*t+l]
-      }
-    }
-
-    CVsMat[i,1] <- bw
-    CVsMat[i,2] <- t(resid)%*%(resid)
-    Pdata$wgt <- NULL
-    resid <- NULL
-    setTxtProgressBar(pb, i)
-  }
-  close(pb)
-
-  bw <- subset(CVsMat, CVsMat[,2]==min(CVsMat[,2], na.rm=TRUE))
-  print(as.data.frame(CVsMat))
-  cat("Optimal value for the bandwidth (number of neighbors): ", bw[,'Bandwidth'], sep="")
-  cat("\nwith a CV score value: ", bw[,'CV-score'], sep="")
-
-  return(bw[,'Bandwidth'])
-}
+# bw.CV.A <- function(formula, data, index, effect=c("individual", "time", "twoways", "nested"),
+#                     model=c("within", "random", "ht", "between", "pooling", "fd"),
+#                     kernel="bisquare", dMat, bws){
+#
+#   #Data preparation
+#   Pdata <- plm::pdata.frame(data, index = index, drop.index = FALSE, row.names = FALSE, stringsAsFactors = default.stringsAsFactors())
+#   n <- length(unique(Pdata[,index[1]]))
+#   t <- length(unique(Pdata[,index[2]]))
+#
+#   CVsMat <- matrix(NA, nrow = length(bws), ncol = 2)
+#   colnames(CVsMat) <- c("Bandwidth", "CV-score")
+#   resid <- c()
+#
+#   # Faire ici le critere de mesure du data pour, si n*t > C, impression d'un message tel quel
+#   # "Long data, calculations may take a long time, optimisation on time-averaged data suggested
+#   # by choosing 'short = TRUE'." et auquel cas moyenner les donnees et les passer dans bw.avg
+#
+#   #Optimization Process
+#   cat("\nOptimization Process:", length(bws), "iterations \n")
+#   pb <- utils::txtProgressBar(min = 0, max = length(bws), style = 3)
+#
+#   for(i in 1:length(bws)){
+#     bw <- bws[i]
+#
+#     for(j in 1:n){
+#       W.j <- GWmodel::gw.weight(as.numeric(dMat[j,]), bw=bw, kernel=kernel, adaptive=T)
+#       W.j[j] <- 0
+#       W.j <- diag(W.j)
+#       W.j <- kronecker(W.j,diag(t))
+#       W.j <- diag(W.j)
+#       Pdata$wgt <- W.j
+#       plm.j <- plm::plm(formula=formula, model=model, data=Pdata, index=index, weights=wgt)
+#
+#       for(l in 1:t){
+#         resid[(j-1)*t+l] <- pmodel.response(plm.j)[(j-1)*t+l] - predict(plm.j)[(j-1)*t+l]
+#       }
+#     }
+#
+#     CVsMat[i,1] <- bw
+#     CVsMat[i,2] <- t(resid)%*%(resid)
+#     Pdata$wgt <- NULL
+#     resid <- NULL
+#     setTxtProgressBar(pb, i)
+#   }
+#   close(pb)
+#
+#   bw <- subset(CVsMat, CVsMat[,2]==min(CVsMat[,2], na.rm=TRUE))
+#   print(as.data.frame(CVsMat))
+#   cat("Optimal value for the bandwidth (number of neighbors): ", bw[,'Bandwidth'], sep="")
+#   cat("\nwith a CV score value: ", bw[,'CV-score'], sep="")
+#
+#   return(bw[,'Bandwidth'])
+# }
 
 
 #' Description:
@@ -136,47 +129,47 @@ bw.CV.A <- function(formula, data, index, effect=c("individual", "time", "twoway
 #' @return double
 #'
 #' @export
-bw.CV.F <- function(formula, data, index, effect=c("individual", "time", "twoways", "nested"),
-                    model=c("within", "random", "ht", "between", "pooling", "fd"),
-                    kernel="bisquare", dMat, interval){
-
-  #Data preparation
-  Pdata <- pdata.frame(data, index = index, drop.index = FALSE, row.names = FALSE,
-                       stringsAsFactors = default.stringsAsFactors())
-  n <- length(unique(Pdata[,index[1]]))
-  t <- length(unique(Pdata[,index[2]]))
-  resid <- c()
-
-  # Faire ici le critere de mesure du data pour, si n*t > C, ou si les bornes sont trop eloignees, impression d'un
-  # message tel quel "Long data, calculations may take a long time, optimisation on time-averaged data suggested
-  # by choosing 'short = TRUE'." et auquel cas moyenner les donnees et les passer dans bw.avg
-
-  #Optimization Process
-  CV <- function(bw, n, kernel, adaptive){
-
-    for(j in 1:n){
-      W.j <- GWmodel::gw.weight(as.numeric(dMat[j,]), bw=bw, kernel=kernel, adaptive=adaptive)
-      W.j[j] <- 0
-      W.j <- diag(W.j)
-      W.j <- kronecker(W.j,diag(t))
-      W.j <- diag(W.j)
-      Pdata$wgt <- W.j
-      plm.j <- plm(formula=formula, model=model, data=Pdata, index=index, weights = wgt)
-
-      for(l in 1:t){
-        resid[(j-1)*t+l] <- pmodel.response(plm.j)[(j-1)*t+l] - predict(plm.j)[(j-1)*t+l]
-      }
-    }
-    CVscore <- t(resid)%*%(resid)
-    return(CVscore)
-  }
-
-  bw.cv <- optimize(CV, interval=interval, n=n, kernel=kernel, adaptive=F, maximum=F)
-  bw <- as.numeric(bw.cv)
-  names(bw) <- c('Bandwidth', 'CV-score')
-  cat("Optimal value for the bandwidth (distance): ", bw[['Bandwidth']], sep="")
-  cat("\nwith a CV score value: ", bw[['CV-score']], sep="")
-
-  return(bw[['Bandwidth']])
-}
+# bw.CV.F <- function(formula, data, index, effect=c("individual", "time", "twoways", "nested"),
+#                     model=c("within", "random", "ht", "between", "pooling", "fd"),
+#                     kernel="bisquare", dMat, interval){
+#
+#   #Data preparation
+#   Pdata <- pdata.frame(data, index = index, drop.index = FALSE, row.names = FALSE,
+#                        stringsAsFactors = default.stringsAsFactors())
+#   n <- length(unique(Pdata[,index[1]]))
+#   t <- length(unique(Pdata[,index[2]]))
+#   resid <- c()
+#
+#   # Faire ici le critere de mesure du data pour, si n*t > C, ou si les bornes sont trop eloignees, impression d'un
+#   # message tel quel "Long data, calculations may take a long time, optimisation on time-averaged data suggested
+#   # by choosing 'short = TRUE'." et auquel cas moyenner les donnees et les passer dans bw.avg
+#
+#   #Optimization Process
+#   CV <- function(bw, n, kernel, adaptive){
+#
+#     for(j in 1:n){
+#       W.j <- GWmodel::gw.weight(as.numeric(dMat[j,]), bw=bw, kernel=kernel, adaptive=adaptive)
+#       W.j[j] <- 0
+#       W.j <- diag(W.j)
+#       W.j <- kronecker(W.j,diag(t))
+#       W.j <- diag(W.j)
+#       Pdata$wgt <- W.j
+#       plm.j <- plm(formula=formula, model=model, data=Pdata, index=index, weights = wgt)
+#
+#       for(l in 1:t){
+#         resid[(j-1)*t+l] <- pmodel.response(plm.j)[(j-1)*t+l] - predict(plm.j)[(j-1)*t+l]
+#       }
+#     }
+#     CVscore <- t(resid)%*%(resid)
+#     return(CVscore)
+#   }
+#
+#   bw.cv <- optimize(CV, interval=interval, n=n, kernel=kernel, adaptive=F, maximum=F)
+#   bw <- as.numeric(bw.cv)
+#   names(bw) <- c('Bandwidth', 'CV-score')
+#   cat("Optimal value for the bandwidth (distance): ", bw[['Bandwidth']], sep="")
+#   cat("\nwith a CV score value: ", bw[['CV-score']], sep="")
+#
+#   return(bw[['Bandwidth']])
+# }
 
--- tests/testthat/test.bwCVF.R
+++ tests/testthat/test.bwCVF.R
@@ -2,38 +2,38 @@
 
 library(testthat)
 
-test_that("Bandwith option: Version avec pseudo-CV / adaptive bw", {
-  load(file = '../../data/Data.Rdata')
-  USStates@data$id <- c(1:length(unique(USStates@data[,"state"])))
-  data <- merge(USStates@data, Produc, by="state", all=T)
-
-  dMat <- GWmodel::gw.dist(sp::coordinates(USStates), p=2, longlat=F)
-  Equation <- log(gsp) ~ log(pcap) + log(pc) + log(emp) + unemp
-
-  ## adaptive bw
-  bwCV.A <-  bw.CV.A(formula=Equation, data=data, index=c("id","year"),
-                     effect='individual', model="within", kernel="bisquare",
-                     dMat=dMat, bws=c(30:40))
-  expected <- c(34.0)
-  names(expected) <- c("bandwidth")
-  expect_setequal(bwCV.A, expected)
-})
-
-
-test_that("Bandwith option: Version avec pseudo-CV / fixed bw", {
-  load(file = '../../data/Data.Rdata')
-  USStates@data$id <- c(1:length(unique(USStates@data[,"state"])))
-  data <- merge(USStates@data, Produc, by="state", all=T)
-
-  dMat <- GWmodel::gw.dist(sp::coordinates(USStates), p=2, longlat=F)
-  Equation <- log(gsp) ~ log(pcap) + log(pc) + log(emp) + unemp
-
-  ## fixed bw
-  bwCV.F <-  bw.CV.F(formula=Equation, data=data, index=c("id","year"),
-                     effect='individual', model="within",
-                     kernel="bisquare", dMat=dMat, interval=c(1500000, 2500000))
-
-  expect_equal(bwCV.F, 2038054.18356155)
-})
+# test_that("Bandwith option: Version avec pseudo-CV / adaptive bw", {
+#   load(file = '../../data/Data.Rdata')
+#   USStates@data$id <- c(1:length(unique(USStates@data[,"state"])))
+#   data <- merge(USStates@data, Produc, by="state", all=T)
+#
+#   dMat <- GWmodel::gw.dist(sp::coordinates(USStates), p=2, longlat=F)
+#   Equation <- log(gsp) ~ log(pcap) + log(pc) + log(emp) + unemp
+#
+#   ## adaptive bw
+#   bwCV.A <-  bw.CV.A(formula=Equation, data=data, index=c("id","year"),
+#                      effect='individual', model="within", kernel="bisquare",
+#                      dMat=dMat, bws=c(30:40))
+#   expected <- c(34.0)
+#   names(expected) <- c("bandwidth")
+#   expect_setequal(bwCV.A, expected)
+# })
+#
+#
+# test_that("Bandwith option: Version avec pseudo-CV / fixed bw", {
+#   load(file = '../../data/Data.Rdata')
+#   USStates@data$id <- c(1:length(unique(USStates@data[,"state"])))
+#   data <- merge(USStates@data, Produc, by="state", all=T)
+#
+#   dMat <- GWmodel::gw.dist(sp::coordinates(USStates), p=2, longlat=F)
+#   Equation <- log(gsp) ~ log(pcap) + log(pc) + log(emp) + unemp
+#
+#   ## fixed bw
+#   bwCV.F <-  bw.CV.F(formula=Equation, data=data, index=c("id","year"),
+#                      effect='individual', model="within",
+#                      kernel="bisquare", dMat=dMat, interval=c(1500000, 2500000))
+#
+#   expect_equal(bwCV.F, 2038054.18356155)
+# })
 
 
