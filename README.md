b7d349b827304e6b045e383fd4b6ecf7e6ab4ed6
David Maignan <davidmaignan@gmail.com>
2020-05-06 10:54:28 -0500

    Add avg test with fixed bw

--- README.md
+++ README.md
@@ -1,2 +1,18 @@
 # Geographically Weighted Panel Regression (GWPR)
 
+Library aims to offer explicit equations for geographically weighted panel regression (GWPR) and R functions to optimize a bandwidth selection in the panel data case and to produce GWPR results and illustrations.
+
+## Requirements
+
+R (>= 4.0)
+
+## Installation
+
+Open a R console or R Studio
+
+```bash
+install.packages("devtools")
+library(devtools)
+devtools.install_github("LAEQ/gwpr")
+??gwpr
+```
--- tests/testthat/test.bw_avg.R
+++ tests/testthat/test.bw_avg.R
@@ -14,3 +14,16 @@ test_that("Bandwith option: Version avec donnees moyennes / adaptive bw ", {
 
   expect_equal(bwAVG.A, 37)
 })
+
+test_that("Bandwith option: Version avec donnees moyennes / fixed bw", {
+  load(file = '../../data/Data.Rdata')
+  USStates@data$id <- c(1:length(unique(USStates@data[,"state"])))
+  data <- merge(USStates@data, Produc, by="state", all=T)
+
+  dMat <- GWmodel::gw.dist(sp::coordinates(USStates), p=2, longlat=F)
+  Equation <- log(gsp) ~ log(pcap) + log(pc) + log(emp) + unemp
+
+  bwAVG.F <- bw.avg(formula=Equation, data=data, SDF=USStates, index=c("id","year"), approach="AICc",  kernel="bisquare", adaptive=F, p=2, longlat=F, dMat=dMat)
+
+  expect_equal(bwAVG.F,2077193.92924246)
+})
