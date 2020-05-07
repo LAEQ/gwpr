c5afd947f89e336744750b3f9bbe47a59d2509b8
David Maignan <davidmaignan@gmail.com>
2020-05-07 10:04:48 -0500

    add unit test bandwith adaptive

--- R/bw_avg.R
+++ R/bw_avg.R
@@ -45,5 +45,72 @@ bw.avg <- function(formula, data, SDF, index, approach=c("CV","AICc"), kernel="b
   return(bw)
 }
 
+#' A function for bandwith selection to calibrate a GWPR model, based on the mean over time of the data.
+
+#' Arguments of the function
+#' @param formula Regression model formula : Y ~ X1 + ... + Xk
+#' @param data dataFrame for the Panel data
+#' @param index List for the indexes : (c(" ID, Time"))
+#' @param effect the effects introduced in the model, one of "individual", "time", or "twoways" (see plm::plm)
+#' @param model one of "pooling", "within", "between", "random", "fd", or "ht" (see plm::plm)
+#' @param kernel gaussian,exponential, bisquare, tricube, boxcar (see GWmodel::gw.weight)
+#' @param dMat a distance matrix or vector (Optional parameter, see GWmodel::gw.weight)
+#' @param bws bandwidths to be used for calculations of CV-score
+#'
+#' @return list(bandwidth = double)
+#'
+#' @export
+bw.CV.A <- function(formula, data, index, effect=c("individual", "time", "twoways", "nested"),
+                    model=c("within", "random", "ht", "between", "pooling", "fd"),
+                    kernel="bisquare", dMat, bws){
 
 
+  #Data preparation
+  Pdata <- plm::pdata.frame(data, index = index, drop.index = FALSE, row.names = FALSE, stringsAsFactors = default.stringsAsFactors())
+  n <- length(unique(Pdata[,index[1]]))
+  t <- length(unique(Pdata[,index[2]]))
+
+  CVsMat <- matrix(NA, nrow = length(bws), ncol = 2)
+  colnames(CVsMat) <- c("Bandwidth", "CV-score")
+  resid <- c()
+
+  # Faire ici le critere de mesure du data pour, si n*t > C, impression d'un message tel quel
+  # "Long data, calculations may take a long time, optimisation on time-averaged data suggested
+  # by choosing 'short = TRUE'." et auquel cas moyenner les donnees et les passer dans bw.avg
+
+  #Optimization Process
+  cat("\nOptimization Process:", length(bws), "iterations \n")
+  pb <- txtProgressBar(min = 0, max = length(bws), style = 3)
+
+  for(i in 1:length(bws)){
+    bw <- bws[i]
+
+    for(j in 1:n){
+      W.j <- GWmodel::gw.weight(as.numeric(dMat[j,]), bw=bw, kernel=kernel, adaptive=T)
+      W.j[j] <- 0
+      W.j <- diag(W.j)
+      W.j <- kronecker(W.j,diag(t))
+      W.j <- diag(W.j)
+      Pdata$wgt <- W.j
+      plm.j <- plm::plm(formula=formula, model=model, data=Pdata, index=index, weights=wgt)
+
+      for(l in 1:t){
+        resid[(j-1)*t+l] <- plm::pmodel.response(plm.j)[(j-1)*t+l] - predict(plm.j)[(j-1)*t+l]
+      }
+    }
+
+    CVsMat[i,1] <- bw
+    CVsMat[i,2] <- t(resid)%*%(resid)
+    Pdata$wgt <- NULL
+    resid <- NULL
+    setTxtProgressBar(pb, i)
+  }
+  close(pb)
+
+  bw <- subset(CVsMat, CVsMat[,2]==min(CVsMat[,2], na.rm=TRUE))
+  print(as.data.frame(CVsMat))
+  cat("Optimal value for the bandwidth (number of neighbors): ", bw[,'Bandwidth'], sep="")
+  cat("\nwith a CV score value: ", bw[,'CV-score'], sep="")
+
+  return(bw[,'Bandwidth'])
+}
--- tests/testthat/test.bw_avg.R
+++ tests/testthat/test.bw_avg.R
@@ -2,7 +2,7 @@
 
 library(testthat)
 
-test_that("Bandwith option: Version avec donnees moyennes / adaptive bw ", {
+test_that("Bandwith option: averages with adaptive bandwith ", {
   Produc <- readRDS(system.file("Produc.rds", package = "gwpr"))
   USStates <- readRDS(system.file("USStates.rds", package = "gwpr"))
 
@@ -18,17 +18,4 @@ test_that("Bandwith option: Version avec donnees moyennes / adaptive bw ", {
   expect_equal(bwAVG.A, 37)
 })
 
-test_that("Bandwith option: Version avec donnees moyennes / fixed bw", {
-  Produc <- readRDS(system.file("Produc.rds", package = "gwpr"))
-  USStates <- readRDS(system.file("USStates.rds", package = "gwpr"))
 
-  USStates@data$id <- c(1:length(unique(USStates@data[,"state"])))
-  data <- merge(USStates@data, Produc, by="state", all=T)
-
-  dMat <- GWmodel::gw.dist(coordinates(USStates), p=2, longlat=F)
-  Equation <- log(gsp) ~ log(pcap) + log(pc) + log(emp) + unemp
-
-  bwAVG.F <- bw.avg(formula=Equation, data=data, SDF=USStates, index=c("id","year"), approach="AICc",  kernel="bisquare", adaptive=F, p=2, longlat=F, dMat=dMat)
-
-  expect_equal(bwAVG.F,2077193.92924246)
-})
