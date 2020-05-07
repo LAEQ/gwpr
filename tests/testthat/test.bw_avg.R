4d63b1666eac41340337e4887b15edac9c1798b9
David Maignan <davidmaignan@gmail.com>
2020-05-07 08:14:35 -0500

    test

--- tests/testthat/test.bw_avg.R
+++ tests/testthat/test.bw_avg.R
@@ -3,12 +3,12 @@
 library(testthat)
 
 test_that("Bandwith option: Version avec donnees moyennes / adaptive bw ", {
-  path <- file.path(system.file("data", package = "gwpr"), "Data.rda")
+  # path <- file.path(system.file("data", package = "gwpr"), "Data.rda")
 
-  if(file.exists(path)){
-    expect_equal(1, 1)
-  } else {
-    expect_equal(1, 0)
-  }
+  test <- readRDS(file.path("..", "..", "data", "test"))
+
+  expect_equal(test, 1)
+
+
 })
 
