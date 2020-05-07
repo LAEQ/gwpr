18882d1a541f5f59383aee8160eb2c61e79a5cea
David Maignan <davidmaignan@gmail.com>
2020-05-07 08:21:26 -0500

    test data

--- tests/testthat/test.bw_avg.R
+++ tests/testthat/test.bw_avg.R
@@ -5,7 +5,7 @@ library(testthat)
 test_that("Bandwith option: Version avec donnees moyennes / adaptive bw ", {
   # path <- file.path(system.file("data", package = "gwpr"), "Data.rda")
 
-  test <- readRDS(file.path("..", "..", "data", "test"))
+  test <- readRDS(system.file("test", package = "gwpr"))
 
   expect_equal(test, 1)
 
