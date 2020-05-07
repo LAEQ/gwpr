bee3e3814a5615dc1004090ace58ec1ab47c3346
David Maignan <davidmaignan@gmail.com>
2020-05-07 10:34:13 -0500

    add unit test for bandwith options

--- tests/testthat/test.bandwith_option.R
+++ tests/testthat/test.bandwith_option.R
@@ -2,24 +2,24 @@
 
 library(testthat)
 
-# test_that("Bandwith option: Version avec pseudo-CV / adaptive bw", {
-#   Produc <- readRDS(system.file("Produc.rds", package = "gwpr"))
-#   USStates <- readRDS(system.file("USStates.rds", package = "gwpr"))
-#
-#   USStates@data$id <- c(1:length(unique(USStates@data[,"state"])))
-#   data <- merge(USStates@data, Produc, by="state", all=T)
-#
-#   dMat <- gw.dist(coordinates(USStates), p=2, longlat=F)
-#   Equation <- log(gsp) ~ log(pcap) + log(pc) + log(emp) + unemp
-#
-#   ## adaptive bw
-#   bwCV.A <-  bw.CV.A(formula=Equation, data=data, index=c("id","year"),
-#                      effect='individual', model="within", kernel="bisquare",
-#                      dMat=dMat, bws=c(30:40))
-#   expected <- c(34.0)
-#   names(expected) <- c("bandwidth")
-#   expect_setequal(bwCV.A, expected)
-# })
+test_that("Bandwith option: Version avec pseudo-CV / adaptive bw", {
+  Produc <- readRDS(system.file("Produc.rds", package = "gwpr"))
+  USStates <- readRDS(system.file("USStates.rds", package = "gwpr"))
+
+  USStates@data$id <- c(1:length(unique(USStates@data[,"state"])))
+  data <- merge(USStates@data, Produc, by="state", all=T)
+
+  dMat <- gw.dist(coordinates(USStates), p=2, longlat=F)
+  Equation <- log(gsp) ~ log(pcap) + log(pc) + log(emp) + unemp
+
+  ## adaptive bw
+  bwCV.A <-  bw.CV.A(formula=Equation, data=data, index=c("id","year"),
+                     effect='individual', model="within", kernel="bisquare",
+                     dMat=dMat, bws=c(30:40))
+  expected <- c(34.0)
+  names(expected) <- c("bandwidth")
+  expect_setequal(bwCV.A, expected)
+})
 #
 # test_that("Bandwith option: Version avec pseudo-CV / fixed bw", {
 #   Produc <- readRDS(system.file("Produc.rds", package = "gwpr"))
