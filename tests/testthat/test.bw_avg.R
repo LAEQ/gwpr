2979f67a36a5c6be1b2c02d1d6c1e75b2a1beec5
David Maignan <davidmaignan@gmail.com>
2020-05-07 07:37:00 -0500

    resolve data file path for unittest

--- tests/testthat/test.bw_avg.R
+++ tests/testthat/test.bw_avg.R
@@ -3,8 +3,8 @@
 library(testthat)
 
 test_that("Bandwith option: Version avec donnees moyennes / adaptive bw ", {
-  wd <- getwd()
-  path <- file.path(getwd(), "..","..", "data", "Data.rda")
+  path <- file.path(system.file("data", package = "gwpr"), "Data.rda")
+
   if(file.exists(path)){
     expect_equal(1, 1)
   } else {
