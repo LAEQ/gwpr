8f47752d37f14e73a54db6ec60892c2007ac7b6f
David Maignan <davidmaignan@gmail.com>
2020-05-08 10:28:47 -0500

    expect equals bandwidth fixed

--- tests/testthat/test.bandwith_option.R
+++ tests/testthat/test.bandwith_option.R
@@ -9,34 +9,31 @@ test_that("Bandwith option: Version avec pseudo-CV / adaptive bw", {
   USStates@data$id <- c(1:length(unique(USStates@data[,"state"])))
   data <- merge(USStates@data, Produc, by="state", all=T)
 
-  dMat <- GWmodel::gw.dist(sp::coordinates(USStates), p=2, longlat=F)
+  dMat <- GWmodel::gw.dist(coordinates(USStates), p=2, longlat=F)
   Equation <- log(gsp) ~ log(pcap) + log(pc) + log(emp) + unemp
 
   ## adaptive bw
   bwCV.A <-  bw.CV.A(formula=Equation, data=data, index=c("id","year"),
                      effect='individual', model="within", kernel="bisquare",
                      dMat=dMat, bws=c(30:40))
-  expect_equal(bwCV.A[['Bandwidth']], 34)
 
-  expect_true(TRUE)
+  expect_equal(bwCV.A[['Bandwidth']], 34)
 })
 
 test_that("Bandwith option: Version avec pseudo-CV / fixed bw", {
-  # Produc <- readRDS(system.file("Produc.rds", package = "gwpr"))
-  # USStates <- readRDS(system.file("USStates.rds", package = "gwpr"))
-  #
-  # USStates@data$id <- c(1:length(unique(USStates@data[,"state"])))
-  # data <- merge(USStates@data, Produc, by="state", all=T)
-  #
-  # dMat <- gw.dist(coordinates(USStates), p=2, longlat=F)
-  # Equation <- log(gsp) ~ log(pcap) + log(pc) + log(emp) + unemp
-  #
-  # ## fixed bw
-  # bwCV.F <-  bw.CV.F(formula=Equation, data=data, index=c("id","year"),
-  #                    effect='individual', model="within",
-  #                    kernel="bisquare", dMat=dMat, interval=c(1500000, 2500000))
+  Produc <- readRDS(system.file("Produc.rds", package = "gwpr"))
+  USStates <- readRDS(system.file("USStates.rds", package = "gwpr"))
 
-  expect_true(TRUE)
+  USStates@data$id <- c(1:length(unique(USStates@data[,"state"])))
+  data <- merge(USStates@data, Produc, by="state", all=T)
 
-  # expect_equal(bwCV.F, 2038054.18356155, tolerance=1e-3)
+  dMat <- gw.dist(coordinates(USStates), p=2, longlat=F)
+  Equation <- log(gsp) ~ log(pcap) + log(pc) + log(emp) + unemp
+
+  ## fixed bw
+  bwCV.F <-  bw.CV.F(formula=Equation, data=data, index=c("id","year"),
+                     effect='individual', model="within",
+                     kernel="bisquare", dMat=dMat, interval=c(1500000, 2500000))
+
+  expect_equal(bwCV.F, 2038054.18356155, tolerance=1e-3)
 })
